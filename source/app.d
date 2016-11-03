0996eda8610a65acb4fc0ff3abdf7d1e69c99298
Robert burner Schadek <rburners@gmail.com>
2016-11-03 11:49:47 +0100

    some more testing, looks good

--- source/app.d
+++ source/app.d
@@ -1,22 +1,6 @@
 import std.stdio;
 
-import std.conv : to;
-
-import parser;
-import lexer;
-import ast;
-
-void main() {
-	string s = "hello[]";
-	auto l = Lexer(s);
-	auto p = new Parser(l);
-
-	Expr e = p.parseExpr();
-	assert(e.ruleSelection == ExprEnum.PostfixExpr);
-
-	PostfixExpr ps = e.post;
-	assert(ps !is null);
-	assert(ps.ruleSelection == PostfixExprEnum.Array,
-		to!string(ps.ruleSelection)
-	);
+void main(string[] args)
+{
+	 /*code*/
 }
